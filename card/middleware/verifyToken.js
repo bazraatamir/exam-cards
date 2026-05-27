bffe8314080293b5d8d9abf982f17ea335321f6c
budaa17 <chingun.chingun1@icloud.com>
2026-05-27 14:31:56 +0800

    task-20

--- card/middleware/verifyToken.js
+++ card/middleware/verifyToken.js
@@ -1,5 +1,6 @@
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
+  
 
   if (!authHeader) {
     return res.status(401).json({ message: "token heregtei" });
