950af302edd3f8d165b91d5262616bb9ec23e458
bazraatamir <69583471+bazraatamir@users.noreply.github.com>
2026-05-27 13:37:17 +0800

    exam-5

--- card/controllers/memberController.js
+++ card/controllers/memberController.js
@@ -13,6 +13,27 @@ const create = async (req, res) => {
   });
 };
 
+const updateController = {
+  async update(req, res, next) {
+    try {
+
+      res.status(200).json(
+        {
+
+        status: 200,
+        message: 'amjilttai shinechlegdlee',
+        data: req.body,
+      });
+
+    } catch (error) {
+
+      next(error);
+    }
+  },
+  
+};
+
 module.exports = { 
-    create 
+    create,
+    updateController 
 };
